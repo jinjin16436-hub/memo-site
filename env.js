71f894d08415050ff476f5f0fc22215def5fcf5c
jinjin16436-hub <jinjin16436@gmail.com>
2025-09-23 00:47:47 +0900

    Update env.js

--- env.js
+++ env.js
@@ -1,14 +1,12 @@
 // env.js
-window.ENV = {
-  // Firebase Web App 설정 (요청한 값 그대로)
-  FIREBASE: {
-    apiKey: "AIzaSyA10dRuTxDs0Ymz0AC1hf7dhn678n4SVs4",
-    authDomain: "my-memo-site.firebaseapp.com",
-    projectId: "my-memo-site",
-    storageBucket: "my-memo-site.firebasestorage.app",
-    messagingSenderId: "196036694705",
-    appId: "1:196036694705:web:8988d12919420130464890"
-  },
+window.firebaseConfig = {
+  apiKey: "AIzaSyA10dRuTxDs0Ymz0AC1hf7dhn678n4SVs4",
+  authDomain: "my-memo-site.firebaseapp.com",
+  projectId: "my-memo-site",
+  storageBucket: "my-memo-site.firebasestorage.app",
+  messagingSenderId: "196036694705",
+  appId: "1:196036694705:web:8988d12919420130464890"
+};
 
   // 읽기 전용 데이터 주인 UID
   PUBLIC_UID: "vv0bADtWdqQUnqFMy8k01dhO13t2",
